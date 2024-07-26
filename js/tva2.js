8b7d205d965c74b14c3defc58926447b1bcbf165
Louca cascalheira-mercier <cascalheiralouca@gmail.com>
2024-07-26 10:33:17 +0200

    js_exo

---
+++ js/tva2.js
@@ -0,0 +1,11 @@
+let ht = prompt("Question: quelle est votre prix hors taxe ?");
+let taxe =prompt  ("choisiser entre 5% 10% 20% de TVA");
+    
+
+if(taxe = ht *1.055){
+document.write(`voici votre resultat ${taxe}`);
+} else if (taxe = ht *1,1){
+document.write(`voici votre resultat ${taxe}`);
+} else if (taxe = ht *1.2){ 
+document.write(`voici votre resultat ${taxe}`);
+}
