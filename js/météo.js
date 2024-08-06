f3d60550f766597dbf03562c8d06ae3a3bc5ca2a
Louca cascalheira-mercier <cascalheiralouca@gmail.com>
2024-08-06 11:51:58 +0200

    js_exo_food

--- js/météo.js
+++ js/météo.js
@@ -1,22 +1,28 @@
-const results = document.querySelector("boxe");
+const APIKEY = "9d14fab5293af26cf8dc10b7ff26e891";
 
-const btn = document.querySelector("#form");
+let city;
+let input = document.querySelector("#city");
 
+input.addEventListener("input", function (e) {
+city = e.target.value;
+// console.log(city);
+// console.log(e);
+weather();
+});
 
-    const apiKey = '9d14fab5293af26cf8dc10b7ff26e891';
-   
-    
-    function getWeatherByCityId(earth, appid) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=earth&appid=${apiKey}&units=metric`;
-    console.log (`https://api.openweathermap.org/data/2.5/weather?q=earth&appid=${apiKey}&units=metric` )
-    fetch(url)
-      .then(response => response.json())
-      .then(data => 
-        (results.innerHTML = `<p>${data.temp}</p>`)
-        (results.innerHTML = `<p>${data.feels}</p>`)
-        (results.innerHTML = `<p>${data.humidity}</p>`)
-        (results.innerHTML = `<p>${data.wind}</p>`)
-      )
+const weather = () => {
+const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`;
 
-      .catch(error => console.error('Error:', error));
-    }
+fetch(URL)
+.then((response) => response.json())
+.then((data) => {
+console.log(data);
+document.querySelector("#temp").innerHTML = `${data.main.temp} °`;
+document.querySelector("#feels").innerHTML = `${data.main.feels_like} °`;
+document.querySelector("#humidity").innerHTML = `${data.main.humidity} %`;
+document.querySelector("#wind").innerHTML = `${data.wind.speed} km/h`;
+})
+.catch((err) => {
+console.log(`Échec de récupération: ${err}`);
+});
+};
